c3cd79d70ff83738e1324e0ae234c958e9ebf86a
Karthikpeddy <peddykarthik1027@gmail.com>
2023-07-28 20:27:01 +0530

    python

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,8 @@ import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+import python from '../assets/python.png';
+
 
 const Skills = () => {
   return (
